ea096520b74517c090743094b639a4bdc1d98035
Veronika Malinouskaya <veronika_malinov@mail.ru>
2022-08-30 14:06:43 +0300

    Задача 41

--- Задача41/Program.cs
+++ Задача41/Program.cs
@@ -3,7 +3,15 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите элементы(через пробел): ");
+Console.Write ("Введите элементы(через пробел): ");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = arr.Count(x => x > 0);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+ if (arr[i] > 0)
+{
+count++;
+}
+}
 Console.WriteLine($"Кол-во элементов > 0: {count}");
